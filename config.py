4857e9e9c8b1cc3bd8b8738608c80f7f2e30bd7d
shraddha <you@example.com>
2023-05-08 09:18:26 +0800

    config file

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+
+#csv_file_path = "file.csv"
+port_number = 5000
+host = "0.0.0.0"
+
+# Model_file_path = os.path.join("files", "linear_model.pkl")
+# Json_file_path = os.path.join("files", "Project_data.json")
+Model_file_path = "Project_app/files/linear_model.pkl"
+Json_file_path = "Project_app/files/Project_data.json"
+# Model_file_path = "linear_model.pkl"
+# Json_file_path = "Project_data.json"
